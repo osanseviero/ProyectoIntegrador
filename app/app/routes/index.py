2a94f59342b6800f677d21419321dc55210904ca
Ivan <ivan@Ivanhelsing.2>
2018-09-05 13:58:28 -0500

    Initial db config

--- app/app/routes/index.py
+++ app/app/routes/index.py
@@ -1,6 +1,9 @@
 from flask import render_template
 from app import app
+from ..db import mongo
 
 @app.route('/')
 def index():
+    # return render_template('index.html')
+    print(mongo)
     return render_template('index.html')
