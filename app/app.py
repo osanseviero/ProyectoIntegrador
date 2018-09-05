856a15f14888986dd396035a0cacc04a4eab2b45
Ivan <ivan@Ivanhelsing.2>
2018-09-04 23:40:51 -0500

    ASDASAD

--- app/app.py
+++ app/app.py
@@ -1,11 +1,10 @@
 # from flask import Flask, render_template, request, jsonify
 from flask import Flask, render_template
 
+app = Flask(__name__)
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-def create_app():
-    app = Flask(__name__)
-    app.register_error_handler(404, page_not_found)
-    return app
+app.register_error_handler(404, page_not_found)
