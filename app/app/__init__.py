1c5d3f60efed38efb1b585950e2253b24e600082
Ivan <ivan@Ivanhelsing.2>
2018-09-05 15:04:33 -0500

    mongo db configuration and session/cookies stuff

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
+# Create flask application object
 app = Flask(__name__)
 
-from .db import *
+# Set session secret key
+app.secret_key = "insert super secret key here"
+
+# Set up mongo database
+mongo = PyMongo(app, uri="mongodb://localhost:27017/WebMLdb")
+
+# Import routes
 from .routes import *
--- app/app/routes/index.py
+++ app/app/routes/index.py
@@ -1,9 +1,6 @@
 from flask import render_template
 from app import app
-from ..db import mongo
 
 @app.route('/')
 def index():
-    # return render_template('index.html')
-    print(mongo)
     return render_template('index.html')
